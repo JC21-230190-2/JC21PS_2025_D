5ffd54798e2ab3bfefaa7a17c8b572dfa2c4b494
230651 <230651@jc-21.jp>
2025-11-27 11:13:39 +0900

    チームDに送る

--- src/main/java/jp/co/jc21ps/activity_management/repository/ParticipantListRepository.java
+++ src/main/java/jp/co/jc21ps/activity_management/repository/ParticipantListRepository.java
@@ -20,8 +20,12 @@ public class ParticipantListRepository {
         /*
          * TODO ➊ 初期表示情報を取得するSQLを完成させる。
          */
-        String sql = """
-
+        String sql = """select a.activity_id, a.user_id, b.activity_name, c.user_name
+                from participant_list_table a
+                join activity_table b on a.activity_id = b.activity_id
+                join user_table c on a.user_id = c.user_id
+                where
+                a.activity_id = ?;
                 """;
 
         List<Map<String, Object>> participantList = jdbcTemplate.queryForList(sql,
